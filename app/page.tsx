8a7dce4b262d21aedca17257d4015b9603e28bfa
Passionhruit <korea_stan@naver.com>
2024-11-12 13:55:05 +0900

    chore: routing crash course

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
+  console.log("Where am I logged?");
   return (
     <Button size="lg" variant="outline">
       Click me!
