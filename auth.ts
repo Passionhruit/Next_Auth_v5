9e229c5e8aaa98a5ae6378dc1a1271d184388dda
Passionhruit <korea_stan@naver.com>
2024-11-25 17:09:05 +0900

    feat: server & client example

--- auth.ts
+++ auth.ts
@@ -64,6 +64,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       if (!existingUser) return token;
 
       token.role = existingUser.role;
+      token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled;
 
       return token;
     },
@@ -78,6 +79,10 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
 
       if (token.role && session.user) {
         session.user.role = token.role;
+      }
+
+      if (session.user) {
+        session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean;
       }
 
       session.customField = "임의로 넣은 필드값";
